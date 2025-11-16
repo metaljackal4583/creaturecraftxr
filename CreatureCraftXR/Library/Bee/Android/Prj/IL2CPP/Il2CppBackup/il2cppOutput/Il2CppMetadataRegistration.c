e43553ec004096c4331e29f6931ddbe31540fdac
RobertaLearning <RobertaLearning@gmail.com>
2025-11-15 21:42:41 -0800

    Rest of Project no .aar

---
+++ CreatureCraftXR/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	20368,
+	g_Il2CppGenericTypes,
+	14551,
+	g_Il2CppGenericInstTable,
+	184777,
+	g_Il2CppGenericMethodFunctions,
+	61248,
+	g_Il2CppTypeTable,
+	215732,
+	g_Il2CppMethodSpecTable,
+	18206,
+	g_FieldOffsetTable,
+	18206,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
